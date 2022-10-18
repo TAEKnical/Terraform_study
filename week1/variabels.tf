33fe22eb8dd2bba98638a9c08edd6bf054420223
TAEKnicl <ilikebigmac@naver.com>
2022-10-19 02:08:27 +0900

    .

---
+++ week1/variabels.tf
@@ -0,0 +1,44 @@
+variable "port" {
+  type = number
+  default = 50000
+}
+
+variable "region" {
+  type = string
+  default = "ap-northeast-2"
+}
+
+variable "profile" {
+  type = string
+  default = "isnt"
+}
+
+variable "key_pair" {
+  type = string
+  default = "isnt"
+}
+
+variable "vpc_cidr" {
+  type = string
+  default = "10.50.0.0/16"
+}
+
+variable "nickname" {
+  type = string
+  default = "isnt"
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+# variable "zone" {
+#   type = string
+#   default = "a"
+# }
+
+# variable "pub_cidr" {
+#   type = string
+#   default = "10.50.0.0/24"
+# }
